5ef8ad478b5a75f9aee1a740e63cd784a704561a
mmathank <mathan.ambition@gmail.com>
2020-03-22 00:57:16 +0530

    Static Resource added

--- src/main/java/sample/mvc/config/WebConfig.java
+++ src/main/java/sample/mvc/config/WebConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
@@ -19,5 +20,10 @@ public class WebConfig implements WebMvcConfigurer {
 		viewResolver.setPrefix("/WEB-INF/jsp/");
 		viewResolver.setSuffix(".jsp");
 		return viewResolver;
+	}
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/images/**").addResourceLocations("/images/");
 	}
 }
--- src/main/webapp/WEB-INF/jsp/welcome.jsp
+++ src/main/webapp/WEB-INF/jsp/welcome.jsp
@@ -1,13 +1,17 @@
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
-    pageEncoding="ISO-8859-1"%>
-<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+	pageEncoding="ISO-8859-1"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
 <title>Spring MVC Application</title>
 </head>
 <body>
-<h2> Spring MVC Home Page</h2>
-<a href="userForm"> Register User </a>
+
+	<img width = 300 height = 200 src="<c:url value="/images/SpringMVC.png" />" /> 
+	<br>
+	<h3>
+		<a href="userForm"> Register User </a>
+	</h3>
 </body>
 </html>
