a4541999c73af7027ff91f26cc525c23f558d639
mmathank <mathan.ambition@gmail.com>
2020-03-23 18:47:49 +0530

    multilingual support added

--- src/main/java/sample/mvc/config/WebConfig.java
+++ src/main/java/sample/mvc/config/WebConfig.java
@@ -1,12 +1,21 @@
 package sample.mvc.config;
 
+import java.util.Locale;
+
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
@@ -25,5 +34,32 @@ public class WebConfig implements WebMvcConfigurer {
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		registry.addResourceHandler("/images/**").addResourceLocations("/images/");
+	}
+
+	@Bean
+	public MessageSource messageSource() {
+		ReloadableResourceBundleMessageSource reloadMessage = new ReloadableResourceBundleMessageSource();
+		reloadMessage.setBasename("classpath:/player");
+		reloadMessage.setDefaultEncoding("UTF-8");
+		return reloadMessage;
+	}
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		LocaleChangeInterceptor interceptor = new LocaleChangeInterceptor();
+		interceptor.setParamName("lang");
+		registry.addInterceptor(interceptor);
+	}
+
+	@Bean
+	public LocaleResolver localeResolver() {
+		SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+		localeResolver.setDefaultLocale(Locale.ENGLISH);
+		return localeResolver;
+	}
+	
+	@Bean
+	public static PropertySourcesPlaceholderConfigurer placeHolderConfigurer() {
+		return new PropertySourcesPlaceholderConfigurer();
 	}
 }
--- src/main/webapp/WEB-INF/jsp/player-form.jsp
+++ src/main/webapp/WEB-INF/jsp/player-form.jsp
@@ -1,7 +1,9 @@
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
 	pageEncoding="ISO-8859-1"%>
 <%@taglib prefix="form" uri="http://www.springframework.org/tags/form"%>
+<%@ taglib prefix="spring" uri="http://www.springframework.org/tags"%>
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<%@ page isELIgnored="false"%>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
 <title>Player Entry Form</title>
@@ -9,12 +11,18 @@
 <body>
 	<form:form action="processPlayerForm" modelAttribute="player">
 		<div align="center">
-			<img width=300 height=200 src="<c:url value="/images/SpringMVC.png" />" />
+			<img width=300 height=200
+				src="<c:url value="/images/SpringMVC.png" />" />
 			<h4>Player Form</h4>
-			<form:label path="firstName">First Name :</form:label>
+			<a href="playerForm?lang=en">English</a> | <a href="playerForm?lang=fr">Française</a> <br> <br>
+			<form:label path="firstName">
+				<spring:message code="label.firstName" />
+			</form:label>
 			<form:input path="firstName" />
 			<br> <br>
-			<form:label path="lastName">Last Name :</form:label>
+			<form:label path="lastName">
+				<spring:message code="label.lastName" />
+			</form:label>
 			<form:input path="lastName" />
 			<br> <br> <input type="submit" value="Submit" />
 		</div>
